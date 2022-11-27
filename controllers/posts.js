f11bca1ab65311f157097ca61bb818fc50d4915f
Nagendra-UI <nagendra.bmail@gmail.com>
2022-11-27 15:28:38 +0530

    configuration

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 import PostMessage from '../models/postMesg.js';
 
 export const getPosts = async (req, res) => {
+
     try {
         const postMessage = await PostMessage.find()
         res.status(200).json(postMessage)
@@ -10,8 +11,10 @@ export const getPosts = async (req, res) => {
 }
 
 export const creatPosts = async (req, res) => {
+
     const post = req.body
     const newPost = new PostMessage(post)
+    console.log(newPost)
     try {
         await newPost.save()
         res.status(201).json({ message: "saved data", data: newPost })
